2533b756db4fbac353c6fa20b2ec38abdfd21e67
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-04-23 15:43:47 -0400

    0.0.3

--- types.d.ts
+++ types.d.ts
@@ -2,12 +2,19 @@ import {BeDecoratedProps} from 'be-decorated/types';
 import {IObserve} from 'be-observant/types';
 export interface BeFormidableVirtualProps{
     invalidIf: FormCriteria[],
-    problems: string[],
+    objections: string[],
+    checkValidityOn: 
 }
 
 export interface FormCriteria{
     noneOf: string[],
-    message: string,
+    instructions: string,
+    invalidMessage: string,
+}
+
+export interface CheckEventMonitor{
+    type: string,
+    options: AddEventListenerOptions,
 }
 
 export interface BeFormidableProps extends BeFormidableVirtualProps{
@@ -16,4 +23,7 @@ export interface BeFormidableProps extends BeFormidableVirtualProps{
 
 export interface BeFormidableActions{
     intro(proxy: HTMLFormElement & BeFormidableVirtualProps, target: HTMLFormElement, beDecorProps: BeDecoratedProps): void;
+    finale(proxy: HTMLFormElement & BeFormidableVirtualProps, target: HTMLFormElement, beDecorProps: BeDecoratedProps): void;
+    onInvalidIf(self: this): void;
+
 }

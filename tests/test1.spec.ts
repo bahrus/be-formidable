01b5ae4b1137da076fbb9dbf4b2fb4bad808beb7
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-04-24 13:17:45 -0400

    0.0.4

---
+++ tests/test1.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test1', async ({ page }) => {
+  await page.goto('http://localhost:3030/tests/test1.html');
+//   const title = page.locator('.navbar__inner .navbar__title');
+//   await expect(title).toHaveText('Playwright');
+  const form = page.locator('form');
+  await expect(form).toHaveClass('invalid');
+});

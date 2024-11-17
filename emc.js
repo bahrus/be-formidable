0b6c31bd0d5af26b34cd7d7e61a8c7c5023292df
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-11-17 12:52:28 -0500

    0.0.17

---
+++ emc.js
@@ -0,0 +1,21 @@
+// @ts-check
+import { BeHive, seed, MountObserver } from 'be-hive/be-hive.js';
+/** @import {EMC} from './ts-refs/trans-render/be/types' */
+
+/**
+ * @type {EMC}
+ */
+export const emc = {
+    base: 'be-formidable',
+    enhPropKey: 'beFormidable',
+    importEnh: async () => {
+        const { BeFormidable } = 
+        /** @type {{new(): IEnhancement<HTMLFormElement>}} */ 
+        /** @type {any} */
+        (await import('./be-formidable.js'));
+        return BeFormidable;
+    }
+};
+
+const mose = seed(emc);
+MountObserver.synthesize(document, BeHive, mose);
